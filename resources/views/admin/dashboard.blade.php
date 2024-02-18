8ebe80d3274e743d9c4fcc4ab5012cdcedbd2fee
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-02-18 13:55:23 +0630

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,7 +11,7 @@
   <meta name="keywords"
     content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-  <title>@yield('title','PM01')</title>
+  <title>@yield('title','PM011')</title>
 
 
 
