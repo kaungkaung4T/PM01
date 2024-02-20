6deb993a094eaa19954a630cbc409716fd554d3f
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-02-21 00:55:41 +0630

    fix calendar

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -4,7 +4,7 @@
         <i class="bi bi-chevron-left"></i>
         </a>
         <div class="navbar-content mt-3">
-            <div style="margin-left: 10px;">
+            <div class="calendar_main">
             <i class="bi bi-calendar4 header-calendar-icon" id="header_cicon"></i>
             <input type="text" name="dates" class="btn btn-sm border header-calendar" id="header_c"/>   <!-- giving specific value="01/01/2018 - 01/15/2018" -->
             <script>
@@ -23,6 +23,47 @@
             <div style="margin-left: 10px;">
             <button class="btn btn-sm border">Reset Date</button>
             </div>
+        </div>
+        
+        <div class="navbar-content2 mt-3">
+          <div class="dropdown">
+            <button class="btn btn-sm border dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Calendar
+            </button>
+            <ul class="dropdown-menu">
+              <li class="mb-2" style="margin-left: 3px;">
+                <div class="calendar_main">
+                  <i class="bi bi-calendar4 header-calendar-icon2 btn btn-sm border" id="header_cicon2"></i>
+                  <input type="text" name="dates2" class="btn btn-sm border header-calendar" id="header_c"/>   <!-- giving specific value="01/01/2018 - 01/15/2018" -->
+                  <script>
+                    $('input[name="dates2"]').daterangepicker();
+
+                    $('#header_cicon2').click(function () {
+                        $('input[name="dates2"]').click();
+                    })
+
+                    $('.dropdown-menu').on('click', function (e) {
+                      e.stopPropagation();
+                    })
+
+                    $('.daterangepicker').on('click', function (e) {
+                      e.stopPropagation();
+                    })
+                  </script>
+                </div>
+              </li>
+              <li class="mb-2" style="margin-left: 3px;">
+                <div>
+                  <button class="btn btn-sm border">Search Date</button>
+                </div>
+              </li>
+              <li class="mb-2" style="margin-left: 3px;">
+                <div>
+                  <button class="btn btn-sm border">Reset Date</button>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
 
