a71cc99bf4bd772972e7e33114800bf767f1199d
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-02-23 04:01:00 +0630

    update

--- database/migrations/2024_02_22_194675_create_customers_table.php
+++ database/migrations/2024_02_22_194675_create_customers_table.php
@@ -11,30 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
+        // Schema::create('customers', function (Blueprint $table) {
+        //     $table->id();
 
-            // One to One on System User Model (default -->>User Model<<--)
-            $table->bigInteger('system_user')->unsigned()->nullable();
-            $table->foreign('system_user')->nullable()->constrained()->references('id')->on('users')->onDelete('cascade');
+        //     // One to One on System User Model (default -->>User Model<<--)
+        //     $table->bigInteger('system_user')->unsigned()->nullable();
+        //     $table->foreign('system_user')->nullable()->constrained()->references('id')->on('users')->onDelete('cascade');
 
-            // One to One on Deposit Model
-            $table->bigInteger('deposit_amount')->unsigned()->nullable();
-            $table->foreign('deposit_amount')->nullable()->constrained()->references('id')->on('deposits')->onDelete('cascade');
+        //     // One to One on Deposit Model
+        //     $table->bigInteger('deposit_amount')->unsigned()->nullable();
+        //     $table->foreign('deposit_amount')->nullable()->constrained()->references('id')->on('deposits')->onDelete('cascade');
 
-            $table->string('username');
-            $table->string('password');
-            $table->string('phone');
-            $table->string('nric');
-            $table->string('name');
-            $table->string('bank_type');
-            $table->string('bank_number');
-            $table->string('remark')->nullable();
-            $table->string('parent_user')->nullable();
-            $table->boolean('fake')->default(false);
-            $table->enum('status', ['Active', 'Inactive'])->default('Active');
-            $table->timestamps();
-        });
+        //     $table->string('username');
+        //     $table->string('password');
+        //     $table->string('phone');
+        //     $table->string('nric');
+        //     $table->string('name');
+        //     $table->string('bank_type');
+        //     $table->string('bank_number');
+        //     $table->string('remark')->nullable();
+        //     $table->string('parent_user')->nullable();
+        //     $table->boolean('fake')->default(false);
+        //     $table->enum('status', ['Active', 'Inactive'])->default('Active');
+        //     $table->timestamps();
+        // });
     }
 
     /**
