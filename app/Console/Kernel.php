0571e07748826cb613e2c1a25ff1606273ff88f7
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-03-15 22:46:39 +0630

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Models\Deposit;
 use App\Models\DepositNoti;
 use App\Models\Subscriptions;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
@@ -38,79 +39,90 @@ class Kernel extends ConsoleKernel
             $all_sub = Subscriptions::all();
             foreach ($all_sub as $each_sub) {
                 if ($each_sub->status == "Active") {
-                    
-                        if ($each_sub->reward_wallet_1) {
-                            $md = Deposit::where("customer_id", $each_sub->customer)->first();
-                            $old_amount = $md->wallet;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
+                    if ($each_sub->reward_wallet_1) {
+                        $md = Deposit::where("customer_id", $each_sub->customer)->first();
+                        $old_amount = $md->wallet;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
         
-                            Deposit::where("customer_id", $each_sub->customer)->update([
-                                'amount' => $plus_amount,
-                                'wallet' => $plus_amount
-                            ]);
+                        Deposit::where("customer_id", $each_sub->customer)->update([
+                            'amount' => $plus_amount,
+                            'wallet' => $plus_amount
+                        ]);
         
-                            $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_1')->first();
-                            $old_amount = $md->wallet1;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
+                        $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_1')->first();
+                        $old_amount = $md->wallet1;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
         
-                            $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_1')->update([
-                                'amount' => $plus_amount,
-                                'wallet1' => $plus_amount,
-                            ]);
-                        }
+                        $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_1')->update([
+                            'amount' => $plus_amount,
+                            'wallet1' => $plus_amount,
+                        ]);
+                    }
 
-                        if ($each_sub->reward_wallet_2) {
-                            $md = Deposit::where("customer_id", $each_sub->customer)->first();
-                            $old_amount = $md->wallet2;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
+                    if ($each_sub->reward_wallet_2) {
+                        $md = Deposit::where("customer_id", $each_sub->customer)->first();
+                        $old_amount = $md->wallet2;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
         
-                            Deposit::where("customer_id", $each_sub->customer)->update([
-                                'amount' => $plus_amount,
-                                'wallet2' => $plus_amount
-                            ]);
+                        Deposit::where("customer_id", $each_sub->customer)->update([
+                            'amount' => $plus_amount,
+                            'wallet2' => $plus_amount
+                        ]);
         
-                            $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_2')->first();
-                            $old_amount = $md->wallet2;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
+                        $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_2')->first();
+                        $old_amount = $md->wallet2;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
         
-                            $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_2')->update([
-                                'amount' => $plus_amount,
-                                'wallet2' => $plus_amount,
-                            ]);
-                        }
-                        
-                        if ($each_sub->reward_wallet_3) {
-                            $md = Deposit::where("customer_id", $each_sub->customer)->first();
-                            $old_amount = $md->wallet3;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
-        
-                            Deposit::where("customer_id", $each_sub->customer)->update([
-                                'amount' => $plus_amount,
-                                'wallet3' => $plus_amount
-                            ]);
-        
-                            $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_3')->first();
-                            $old_amount = $md->wallet3;
-                            $new_amount = $each_sub->package_data->reward_amount;
-                            $plus_amount = $old_amount + $new_amount;
-        
-                            $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_3')->update([
-                                'amount' => $plus_amount,
-                                'wallet3' => $plus_amount,
-                            ]);
-                        }
+                        $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_2')->update([
+                            'amount' => $plus_amount,
+                            'wallet2' => $plus_amount,
+                        ]);
+                    }
                     
+                    if ($each_sub->reward_wallet_3) {
+                        $md = Deposit::where("customer_id", $each_sub->customer)->first();
+                        $old_amount = $md->wallet3;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
+        
+                        Deposit::where("customer_id", $each_sub->customer)->update([
+                            'amount' => $plus_amount,
+                            'wallet3' => $plus_amount
+                        ]);
+        
+                        $md = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_3')->first();
+                        $old_amount = $md->wallet3;
+                        $new_amount = $each_sub->package_data->reward_amount;
+                        $plus_amount = $old_amount + $new_amount;
+        
+                        $all_deposit = DepositNoti::where("customer_id", $each_sub->customer)->where('wallet', '=', 'wallet_3')->update([
+                            'amount' => $plus_amount,
+                            'wallet3' => $plus_amount,
+                        ]);
+                    }
                 }
             }
-            
-            
+        })->dailyAt('22:43')->timezone('Asia/Rangoon');
+
+        $schedule->call(function () {
+            $all_sub = Subscriptions::all();
+            foreach ($all_sub as $each_sub) {
+                if ($each_sub->status == "Active") {
+                    
+                    $today = Carbon::now();
+                    
+                    if ($today === $each_sub->end_at) {
+                        $each_sub->status = "Inactive";
+                        $each_sub->save();
+                    }
+                }
+            }
         })->everyMinute();
-        // daily()
+
     }
 
     /**
