5e9fbe751c0ada3b2d95226927cc49e1d461ce67
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-03-21 02:33:40 +0630

    update

--- app/Http/Controllers/FrontEnd/Index.php
+++ app/Http/Controllers/FrontEnd/Index.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Package;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Index extends Controller
 {
     public function index (Request $request) {
 
         $packages = Package::all();
+        $customer_id = Auth::guard('customer')->id();
+        $customer = Customer::find($customer_id);
 
         $context = [
-            'packages' => $packages 
+            'packages' => $packages,
+            'customer' => $customer
         ];
 
         return view('front_end.index', $context);
